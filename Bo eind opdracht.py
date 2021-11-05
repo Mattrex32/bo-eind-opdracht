dd96eb2e79f779e56807b3081912b07196215587
Adnan <adnanrajeh2003@gmail.com>
2021-11-05 11:24:17 +0100

    Eind

---
+++ Bo eind opdracht.py
@@ -0,0 +1,67 @@
+print("Waar is Adnan Geboren? \n A Damascus. \n B Kafrhower.")
+geboren = input("Voeg je antwoord hier:")
+if geboren == "A" or geboren == "a" or geboren == " A" or geboren == " a":
+    print("Adnan is in Damacus geboren,\nna 8 jaar is het oorlog begonnen in syrië,\nAdnan Was klein, Heeft adnan goed leven toen hij klein was?")
+    boventweede = input("A beetje\nB Niet\nVoeg je antwoord hier:")
+    if boventweede == "A" or boventweede == "a" or boventweede == " A" or boventweede == " a":
+        print("In damacus was het niet helemaal goed,\nmaar het was beter dan andere pleken in Syrië,\nmoet adnan vertreken uit Damacus of blijven?\nA Blijven\nB Vertreken")
+        eerstderde = input("Voeg je antwoord hier:")
+        if eerstderde == "A" or eerstderde == "a" or eerstderde == " A" or eerstderde == " a":
+            print("Adnan is gebleven Damacus,\nzijn vader zei: Adnan of ga mee naar Nederland\nof blijf je hier in je land\n en je school volgt\nA Blijven in Syrië.\nB naar nederland gaan")
+            eerstevierde = input("Voeg je antwoord hier:")
+            if eerstevierde == "A" or eerstevierde == "a" or eerstevierde == " A" or eerstevierde == " a":
+                print("Helaas Adnan werd op gepakt en hij moest vegten met de legger mee. ):")
+            elif eerstevierde == "B" or eerstevierde == "b" or eerstevierde == " B" or eerstevierde == " b":
+                print("Gelukkig, Adnan heeft nieuw leven die veilig is, en een goed toekomst. (:")
+        elif eerstderde == "B" or eerstderde == "b" or eerstderde == " B" or eerstderde == " b":
+            print("Adnan is naar andere plek vertroken die\nerger is dan Damacus. zijn vader zei: Adnan\nof ga mee naar Nederland of blijf\nje hier met je oma, opa tante en ..\nA Blijven in Syrië\nB gaan naar Nederland")
+            tweevierde = input("Voeg je antwoord hier:")
+            if tweevierde == "A" or tweevierde == "a" or tweevierde == " A" or tweevierde == " a":
+                print("Adnan is dood geschoten door de legger,\nomdat hij met zijn opa en oma wou blijvne ):")
+            elif tweevierde == "B" or tweevierde == "b" or tweevierde == " B" or tweevierde == " b":
+                print("Gelukkig, Adnan heeft nieuw leven die veilig is,\nen een goed toekomst. (:, alleen hij ziet\nzijn opa en oma niet meer zien }:")
+    elif boventweede == "B" or boventweede == "b" or boventweede == " B" or boventweede == " b":
+        print("In damacus was het niet goed,\nmoet adnan vertreken uit Damacus of blijven?\nA Blijven\nB Vertreken")
+        tweederde = input("Voeg je antwoord hier:")
+        if tweederde == "A" or tweederde == "a" or tweederde == " A" or tweederde == " a":
+            print("Adnan is gebleven in Damacu\n, maar hij moest in de dienst\nA mee gaan\nB Niet mee gaan ")
+            derdevier = input("Voeg je antwoord hier:")
+            if derdevier == "A" or derdevier == "a" or derdevier == " A" or derdevier == " a":
+                print("Hij moest mensen dood schieten,\ndus hij heeft zich zelf dood geschoten zo\ndat hij zijn eigen volk niet dood schiet ):")
+            elif derdevier == "B" or derdevier == "b" or derdevier == " B" or derdevier == " b":
+                print("Adnan is dood geschoten\ndoor de legger, omdat hij\nniet mee wou gaan ):")
+        elif tweederde == "B" or tweederde == "b" or tweederde == " B" or tweederde == " b":
+            print("Adnan Moest vertreken naar andere plek,\nhij had 2 keuzes.\nA Naar zijn oma in Damacus\nB Naar Nederland")
+            vierdevier = input("Voeg je antwoord hier:")
+            if vierdevier == "A" or vierdevier == "a" or vierdevier == " A" or vierdevier == " a":
+                print("Helaas hij is Dood door een pom")
+            elif vierdevier == "B" or vierdevier == "b" or vierdevier == " B" or vierdevier == " b":
+                print("Gelukkig, Adnan heeft\nnieuw leven die veilig is,\nen een goed toekomst. (:")
+
+elif geboren == "B" or geboren == "b" or geboren == " B" or geboren == " b":
+    print("Adnan is in Kafrhowr geboren,\nna 8 jaar is het oorlog begonnen in syrië,\nAdnan Was klein, Was er oorlog in Kafrhowr?\nA Er was wel oorlog in Kafrhowr.\nB Nee er was niks ")
+    ondertweede = input("Voeg je antwoord hier:")
+    if ondertweede == "A" or ondertweede == "a" or ondertweede == " A" or ondertweede == " a":
+        print("In Kafrhower was er wel oorlog en het\nwas erger dan Damacus, omdat het een\ndorpje was, moet adnan naar\nDamacus vertreken of niet?\nA vertreken\nB Blijven")
+        derdedrie = input("Voeg je antwoord hier:")
+        if  derdedrie == "A" or derdedrie == "a" or derdedrie == " A" or derdedrie == " a":
+            print("In Damacus was het ook erg,\nmoet hij daar blijven \nof naar NL vertreken?\nA Blijven\nB Vertreken")
+            vijfdevier = input("Voeg je antwoord hier:")
+            if vijfdevier == "A" or vijfdevier == "a" or vijfdevier == " A" or vijfdevier == " a":
+                print("Helaas hij is Dood door een pom")
+            elif vijfdevier == "B" or vijfdevier == "b" or vijfdevier == " B" or vijfdevier == " b":
+                print("Gelukkig, Adnan heeft nieuw leven die veilig is, en een goed toekomst. (:")
+        elif derdedrie == "B" or derdedrie == "b" or derdedrie == " B" or derdedrie == " b":
+            print("Helaas hij is Dood door een pom")
+    elif ondertweede == "B" or ondertweede == "b" or ondertweede == " B" or ondertweede == " b":
+        print("Er was niks in Kafrhower tot 2012,\ndaarna begon het oorlog,\nWat moet Adnan Doen ?\nA Blijfen In Kafrhower?\nB Vertreken naar De grenzen Syrië/Lebanon?")
+        vierdedrie = input("Voeg je antwoord hier:")
+        if vierdedrie == "A" or vierdedrie == "a" or vierdedrie == " A" or vierdedrie == " a":
+            print("Adnan is gebleven in Kafrhower, maar het\noorlog wrodt alleen maar erger ]:\nA vegten voor zijn eigen veiligheid\nB Verstopen van de legger")
+            zevendevier = input("Voeg je antwoord hier:")
+            if zevendevier == "A" or zevendevier == "a" or zevendevier == " A" or zevendevier == " a":
+                print("Helaas hij is geschoten ):")
+            elif zevendevier == "B" or zevendevier == "b" or zevendevier == " B" or zevendevier == " b":
+                print("Helaas hij is op gepakt na een maand ):")
+        elif vierdedrie == "B" or vierdedrie == "b" or vierdedrie == " B" or vierdedrie == " b":
+            print("Hij is dood geschoten door een sniper ):")
